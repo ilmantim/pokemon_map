4fa8b6efb4ab9d9e66a34202be1523c631d92150
ilmantim <ilmantim@gmail.com>
2023-09-29 17:45:47 +0200

    add related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -14,7 +14,7 @@ class Pokemon(models.Model):
     
     
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, verbose_name='Покемон')
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True, verbose_name='Покемон', related_name='entities')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Время появления')  
